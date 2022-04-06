4e0e7f5293bc87deac6d627b1cd08fd1bd3130aa
lwood54 <lwood3499@gmail.com>
2022-04-05 21:22:13 -0500

    initialize

--- KotlinExercise1/src/KotlinPerson.kt
+++ KotlinExercise1/src/KotlinPerson.kt
@@ -12,13 +12,15 @@ data class KotlinPerson (val id: Long, val title: String, val firstName: String,
             return years
         }
     }
-    fun getAge() = getAge(dateOfBirth)
+    val age: Int
+    get() = getAge(dateOfBirth)
 }
 
 fun main(args: Array<String>) {
     val john = KotlinPerson(1L, "Mr", "John", "Blue", GregorianCalendar(1977, 9, 3));
     val jane = KotlinPerson(2L, "Mrs", "Jane", "Green", null);
-    println("$john's age is ${john.getAge()}")
-    println("$jane's age is ${jane.getAge()}")
+    println("$john's age is ${john.age}")
+    println("$jane's age is ${jane.age}")
     println("The age of someone born on 3rd May 1988 is " + KotlinPerson.getAge(GregorianCalendar(1988, 5, 3)));
+    val olderPerson = if (john.age > jane.age)
 }
